38fc81ac434dd61a948655c14e8e327233e1bc9a
Tri <phamminhtri26102003@gmail.com>
2025-07-09 15:50:36 +0700

    fix lại token trong login google

--- course-recommendation/app/Http/Middleware/JwtCookieMiddleware.php
+++ course-recommendation/app/Http/Middleware/JwtCookieMiddleware.php
@@ -12,19 +12,19 @@ class JwtCookieMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        // Kiểm tra token từ header
-        try {
-            if (JWTAuth::parseToken()->authenticate()) {
-                return $next($request);
-            }
-        } catch (TokenInvalidException $e) {
-            // Token không hợp lệ, tiếp tục kiểm tra cookie
-        } catch (TokenExpiredException $e) {
-            // Token hết hạn
-            return response()->json(['error' => 'Token expired'], 401);
-        } catch (\Exception $e) {
-            // Không có token trong header, tiếp tục kiểm tra cookie
-        }
+        // // Kiểm tra token từ header
+        // try {
+        //     if (JWTAuth::parseToken()->authenticate()) {
+        //         return $next($request);
+        //     }
+        // } catch (TokenInvalidException $e) {
+        //     // Token không hợp lệ, tiếp tục kiểm tra cookie
+        // } catch (TokenExpiredException $e) {
+        //     // Token hết hạn
+        //     return response()->json(['error' => 'Token expired'], 401);
+        // } catch (\Exception $e) {
+        //     // Không có token trong header, tiếp tục kiểm tra cookie
+        // }
 
         // Kiểm tra token từ cookie
         $token = $request->cookie('jwt_token');
